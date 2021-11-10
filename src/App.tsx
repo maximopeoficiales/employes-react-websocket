52e959a2ff7947eaae56b0b76e5ba80d0f4837cf
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-11-10 12:17:25 -0500

    ADD: conexion con socket exitos

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,45 @@
-import EmployeAdd from "app/presentation/components/EmployeAdd/EmployeAdd";
-import EmployeList from "app/presentation/components/EmployeList/EmployeList";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import EmployeAdd from "./app/presentation/components/EmployeAdd/EmployeAdd";
+import EmployeList from "./app/presentation/components/EmployeList/EmployeList";
+import { config } from "./config";
 
+const connectSocketServer = () => {
+  return io(config.host_socket, { transports: ["websocket"] });
+};
 function App() {
+  const [socket, setSocket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      setOnline(true);
+    });
+
+    // return socket.disconnect();
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("disconnect", () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
   return (
     <div className="App">
       <div className="container">
         <div className="d-flex justify-content-end my-3 ">
           <p className="text-white">
             <b>Service Status:</b>
-            <span className="badge bg-success mx-2">Online</span>
-            {/* <span className="badge bg-danger mx-2">Oflline</span> */}
+            {online ? (
+              <span className="badge bg-success mx-2">Online</span>
+            ) : (
+              <span className="badge bg-danger mx-2">Oflline</span>
+            )}
           </p>
         </div>
         <h1 className="my-4 text-center">Employes in Real Time</h1>
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const config = {
+    host_socket: 'http://localhost:8083',
+}
